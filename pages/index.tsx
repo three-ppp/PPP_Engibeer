d81767af978869ed800ae83bdcae961805833d8d
y inoue <admin@wainoMacBook-Pro.local>
2021-10-16 00:22:35 +0900

    first commit yururin

--- pages/index.tsx
+++ pages/index.tsx
@@ -53,6 +53,8 @@ const Home: NextPage = () => {
         </div>
       </main>
 
+      test
+
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
