042415f455d552b51f81827cf383c939ca9a5899
london-newyork <cactus1272004@yahoo.co.jp>
2021-11-22 22:20:26 +0900

    Props型宣言の追加・削除、Propsの内容と三項演算子の中身の変更

--- components/DisplayBoardEngibeer/DisplayBoardEngibeer.tsx
+++ components/DisplayBoardEngibeer/DisplayBoardEngibeer.tsx
@@ -1,35 +1,25 @@
 import { VFC } from "react";
 import Avatar from "../Avatar";
 import styles from "./displayboardEngibeer.module.css";
-import HeyButton from "../HeyButton/index";//読み取られていない
 
 type Props = {
-    text: string;
+    title: string;
     displayname: string;
     iconURL: string;
-    engibeerCount?: number;
-    engibeer?: string;
-    count?: number;
-    countUser?: number;
-    heyCount?: number;
+    totalHeyCount?: number;
+    engibeerNumber?: number;
 }
 
 const DisplayBoardEngibeer:VFC<Props> = (props) => {
-const { displayname, iconURL, engibeerCount, engibeer, count } = props;
-const countUser = [];//へえ押した人数を把握するためにuserId入れる
-const heyCount = [count];//HeyButtonからへえ数を配列へ入れる
+const { title, displayname, iconURL, totalHeyCount, engibeerNumber } = props;
+
     return(
         <>
-        {engibeer && engibeerCount ? (
-            //desktop16の時は以下を返す
-            <ul className={styles.displayBoardAdj}>
-                <li className={styles.displayBoardText}>{engibeer}</li>
-                <li className="flex justify-start pt-6 items-center"><Avatar displayname={displayname} iconURL={iconURL}/></li>
-            </ul>
-        ):(
+        {title && engibeerNumber ? (
+            //desktop13の時は以下を返す
             <ul className={styles.displayBoardForLiveAdj}>
-                <li className={styles.displayBoardNum}>エンジビア{engibeerCount}</li>
-                <li className={styles.displayBoardText}>{engibeer}</li>
+                <li className={styles.displayBoardNum}>エンジビア{engibeerNumber}</li>
+                <li className={styles.displayBoardText}>{title}</li>
                 <li>
                     <ul className="flex justify-between items-end pt-6">
                         <li className="flex items-center">
@@ -37,17 +27,16 @@ const heyCount = [count];//HeyButtonからへえ数を配列へ入れる
                         </li>
                             {/* //HeyButtonの押された数が集計されてDisplayHey(仮)コンポーネントに入ってくる。集計された数は上限100まで */}
                         <li className={styles.displayHey}>
-                            {/* {()=>{
-                            for(count=0; count<100; count++){
-                                return(
-                                    ({countUser} / {heyCount}) * 5;
-                                )
-                            }}
-                            } */}
+                            {totalHeyCount}
                             <small>へえ</small>
                         </li>
                     </ul>
                 </li>
+            </ul>
+        ):(
+            <ul className={styles.displayBoardAdj}>
+                <li className={styles.displayBoardText}>{title}</li>
+                <li className="flex justify-start pt-6 items-center"><Avatar displayname={displayname} iconURL={iconURL}/></li>
             </ul>
         )
         } 
