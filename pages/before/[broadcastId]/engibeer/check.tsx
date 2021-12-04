ffabe46887b5198a596c380b02092ecf4bbc746e
oshita-n <oshita-n@whispon.com>
2021-12-05 03:22:34 +0900

    🔨 確認と編集のページを途中まで作成

---
+++ pages/before/[broadcastId]/engibeer/check.tsx
@@ -0,0 +1,80 @@
+import { useRouter } from "next/dist/client/router";
+import { useCallback, useEffect, useState, VFC } from "react";
+import BroadcastTitle from "../../../../components/BroadcastTitle";
+import Button from "../../../../components/Button";
+
+type BroadcastInfo = {
+  id: string;
+  title: string;
+  status: "before" | "during" | "after";
+  engibeerCount: string;
+  date: string;
+};
+
+const EngiberrAddPage: VFC = () => {
+  const [broadcastInfo, setBroadcastInfo] = useState<BroadcastInfo>();
+  const [engibeer, setEngibeer] = useState("");
+
+  const router = useRouter();
+
+  const fetchLiveInfo = useCallback(
+    async (broadcastId: string | string[] | undefined) => {
+      const res = await fetch(
+        `http://localhost:3001/broadcasts/${broadcastId}`
+      );
+      const json = await res.json();
+      setBroadcastInfo(json);
+    },
+    []
+  );
+
+  const editEngibeer = useCallback(() => {
+    // エンジビアを編集する処理を書く
+    console.log(engibeer);
+  }, [engibeer]);
+  
+  const removeEngibeer = useCallback(() => {
+    // エンジビアを削除する処理を書く
+    console.log(engibeer);
+  }, [engibeer]);
+
+  useEffect(() => {
+    // クエリがセットされたことを検知
+    if (router.asPath !== router.route) {
+      const { broadcastId } = router.query;
+      fetchLiveInfo(broadcastId);
+    }
+  }, [router]);
+
+  return (
+    <div>
+			<div className="flex flex-col">
+				{broadcastInfo !== undefined ? (
+					<div className="pt-10 mb-8">
+						<BroadcastTitle
+							title={broadcastInfo.title}
+							status={broadcastInfo.status}
+						/>
+					</div>
+				) : null}
+
+
+				<div className="mb-auto">
+					<p className="text-3xl bg-white p-8 rounded-lg w-2/5 text-center mx-auto mt-8">
+						HTMLにはポータルという便利な要素がある
+					</p>
+				</div>
+
+				<div className="flex flex-row justify-center items-center mt-8">
+					<Button text="編集する" onClick={editEngibeer} />
+					<Button text="削除する" onClick={removeEngibeer} />
+				</div>
+      
+
+    	</div>
+		</div>
+
+  );
+};
+
+export default EngiberrAddPage;
