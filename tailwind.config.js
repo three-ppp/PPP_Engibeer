d37492d01171d5a6aa80cf90be25e895c6b5ebd4
takada <60991332+tksat@users.noreply.github.com>
2021-12-11 22:19:10 +0900

    ログインページの作成 (#57)
    * ✨ svg画僧をコンポーネントで可能にbabel-plugin-inline-react-svg導入
    
    * #5 ログインページの作成
    
    * babel-plugin-inline-react-svgをdocker内でインストールに変更
    
    * ♻️ spg画像をpng画像に置き換える
    
    * 🔥 babel-plugin-inline-react-svgをuninstallしました
    
    * ♻️ imgタグでsrcに直接urlを指定する方法に変更
    
    * 🔥 babel-plugin-inline-react-svgをuninstallしました
    
    * マージした際のコンフリクト解消
    
    * signin.jsxの誤字修正
    
    * ♻️ tailwind.config.js内アロー関数に変更
    
    * singin画像のリンクを修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
     extend: {},
   },
   theme: {
+    backgroundImage: (theme) => ({
+      'signin-img': "url('/bg-img.png')",
+        }),
     backgroundColor: (theme) => ({
       ...theme("colors"),
       orange: "#FFEDD5",
@@ -26,5 +29,14 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    ({ addUtilities }) => {
+      const newUtilities = {
+        ".text-shadow": {
+          textShadow: "2px 0 0 #FBBF24, 0 2px 0 #FBBF24, -2px 0 0 #FBBF24, 0 -2px 0 #FBBF24"
+        }
+      };
+      addUtilities(newUtilities);
+    }
+  ],
 };
